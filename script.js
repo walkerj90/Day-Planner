8bfa4bf87a459ec2e416e81279d8a69f7da66b96
Justin <walkerj90@icloud.com>
2021-01-24 15:53:43 -0800

    initial commit with very small changes made

---
+++ script.js
@@ -0,0 +1,25 @@
+const business = { start: 9, end: 18 }
+var dt = DateTime.local();
+
+// Add the current date to top of screen
+//https://moment.github.io/luxon/docs/manual/tour.html#your-first-datetime
+dt = DateTime.
+
+    // Get the current hour
+
+
+    // Get values from local storage
+
+
+    // Build the time slots
+
+
+
+
+    // Update display at the start of the next hour
+
+
+    // Get the current hour
+
+
+    // Update each color
